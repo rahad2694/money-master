23e5884eb7e50f1dd66b8218c4a757e9dcf4150f
rahad2694 <rahad.haw@gmail.com>
2022-02-17 10:38:32 +0600

    error handled

--- js/app.js
+++ js/app.js
@@ -9,14 +9,38 @@ const saveAmount = document.getElementById('save-amount');
 const balanceAfterSave = document.getElementById('balance-after-save');
 
 function updateTotal(){
+    erroHandle('income');
+    erroHandle('food');
+    erroHandle('rent');
+    erroHandle('clothes');
+    
     let monthlyCost = updateExpenses();
     totalExpenses.innerText = monthlyCost;
     balanceBeforeSave.innerText = Number(incomeInputBox.value) - monthlyCost;
 }
-function updateExpenses(){
-    if(isNaN(incomeInputBox.value)){
-        
+
+function erroHandle(targetField){
+    const targetFieldBoxId = targetField + '-input-box';
+    const targetFieldBox = document.getElementById(targetFieldBoxId); 
+    const stringAlertId = 'string-alert-in-'+targetField;
+    const negativeAlertId = 'negative-alert-in-'+targetField;
+    if(targetFieldBox.value < 0){
+        document.getElementById(negativeAlertId).classList.remove('d-none');
+        document.getElementById(stringAlertId).classList.add('d-none');
+        targetFieldBox.value ='';
+        return;
+    } else if (isNaN(targetFieldBox.value)){
+        document.getElementById(stringAlertId).classList.remove('d-none');
+        document.getElementById(negativeAlertId).classList.add('d-none');
+        targetFieldBox.value ='';
+        return;
+    } else if (targetFieldBox.value >0){
+        document.getElementById(stringAlertId).classList.add('d-none');
+        document.getElementById(negativeAlertId).classList.add('d-none');
+        // return;
     }
+}
+function updateExpenses(){
     const expenses = Number(foodInputBox.value) + Number(rentInputBox.value) + Number(clothesInputBox.value);
     return expenses;
 }
